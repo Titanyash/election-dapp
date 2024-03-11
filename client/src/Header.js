6beecb6017ca740a887361d4adf91922a4e934d9
Titanyash <162488294+Titanyash@users.noreply.github.com>
2024-03-12 01:24:34 +0300

    Update Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 function Header() {
     return (
         <div className="main">
-            <h1>The Poll Booth</h1>
+            <h1>Blockhain голосование</h1>
         </div>
     )
 }
